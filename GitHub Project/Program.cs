a42a2c0d3d2c7d52c219240da85da585d5a7aafb
NicKerin <nicolekerin.jipa@gmail.com>
2023-10-05 15:07:50 -0400

    Primeiro Commit

---
+++ GitHub Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num + num2;
+        Console.WriteLine($"A soma dos números é {result}.");
+
+        Console.ReadKey();
+    }
+}
